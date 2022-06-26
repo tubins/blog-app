a5c706fd21524bc58cf7496c5faeb99a7f97f60e
tubins <tubin.shaji@gmail.com>
2022-06-26 14:12:33 +0100

    Add ResourceNotFound exception

---
+++ src/main/java/com/tubz/blog/blogapp/exceptions/ResourceNotFound.java
@@ -0,0 +1,25 @@
+package com.tubz.blog.blogapp.exceptions;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+@Getter
+public class ResourceNotFound extends RuntimeException {
+    private String resourceName;
+    private String fieldName;
+    private String fieldValue;
+
+    /**
+     * Constructs a new runtime exception with {@code null} as its
+     * detail message.  The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause}.
+     */
+    public ResourceNotFound(String resourceName, String fieldName, String fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
+    }
+}
